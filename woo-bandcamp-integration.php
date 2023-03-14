acff2fd9078df3779ebfce35f20609cb7670784b
andrewklimek <andrew.klimek@gmail.com>
2023-03-14 14:34:10 +0000

    turn off cron fix

--- woo-bandcamp-integration.php
+++ woo-bandcamp-integration.php
@@ -21,8 +21,8 @@ defined('ABSPATH') || exit;
  */
 
 // TEMP
-add_action('wp', __NAMESPACE__ .'\fix_cron');
-add_action( 'mnmlbc2wc_main_cron_hook', '__return_true' );
+// add_action('wp', __NAMESPACE__ .'\fix_cron');
+// add_action( 'mnmlbc2wc_main_cron_hook', '__return_true' );
 
 add_action( 'bandcamp_woo_periodic_fetch', __NAMESPACE__ .'\main_process' );
 add_action( 'bandcamp_woo_retry_mark_shipped', __NAMESPACE__ .'\retry_add_tracking' );
